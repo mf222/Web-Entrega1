938894577fe53745f7f4fc6db880f639e2708d1f
Bastián Mavrakis Córdova <bamavrakis@uc.cl>
2016-03-13 23:50:06 -0300

    Cambios en versión de paperclip  y en la validación

--- app/models/explorer.rb
+++ app/models/explorer.rb
@@ -1,5 +1,7 @@
 class Explorer < ActiveRecord::Base
 	has_attached_file :image 
+	validate_media_type :false
 	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+	validates_attachment_content_type :image, :content_type => [/\Aimage/, 'application/octet-stream']
     #do_not_validate_attachment_file_type :image
 end
