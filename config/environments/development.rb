8516660e1fc5516996aef000131d383def344711
Bastián Mavrakis Córdova <bamavrakis@uc.cl>
2016-03-13 22:39:24 -0300

    fix images

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  Paperclip.options[:command_path] = 'C:\Program Files(x86)\GnuWin32\bin'
+  Paperclip.options[:command_path] = 'C:\Program Files(x86)\GnuWin32'
+
   # Do not eager load code on boot.
   config.eager_load = false
 
